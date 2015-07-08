cbdfd3ab1533261ea6e5642791ec8d91a9f1a019
zdp757142517 <757142517@qq.com>
2015-07-08 10:19:09 +0800

    Revert "工程"
    This reverts commit e23c6795641d02afa861724fac39f7631264d4d0.

--- gen/com/bigdat/clinic/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.bigdat.clinic;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/bigdat/clinic/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.bigdat.clinic;
-
-import android.app.Activity;
-
-public class MainActivity extends Activity {
-
-}
